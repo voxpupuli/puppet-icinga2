4c68e699219e9aa428c1efcf80e394982d82805c
Nick Chappell <nick@intronic.org>
2014-05-02 19:36:32 -0700

    Initial commit.

---
+++ manifests/init.pp
@@ -0,0 +1,18 @@
+# == Class: icinga2
+#
+# This module installs the Icinga 2 monitoring system.
+#
+# === Parameters
+#
+# Coming soon...
+#
+# === Examples
+# 
+# Coming soon...
+# 
+
+#Our base class.
+
+class icinga2 {
+  
+}
