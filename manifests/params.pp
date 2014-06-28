3e959201cd2b5045b197f7440da4d0ca828a4f9c
Nick Chappell <nick@intronic.org>
2014-06-27 21:39:42 -0700

    Added a skeleton icinga2::params class.

---
+++ manifests/params.pp
@@ -0,0 +1,12 @@
+# == Class: icinga2::params
+#
+# This class contains config options and settings for use elsewhere in the module.
+#
+# === Parameters
+#
+# See the inline comments.
+#
+
+class icinga2::params {
+
+}
