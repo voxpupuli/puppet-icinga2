ba90a87f8c03a7b8df7f3e74116dd32efd02d05b
Nick Chappell <nick@intronic.org>
2014-06-27 21:40:02 -0700

    Added skeleton icinga2::server classes and subclasses.

---
+++ manifests/server.pp
@@ -0,0 +1,16 @@
+# == Class: icinga2::server
+#
+# This module installs and configures the server components for the Icinga 2 monitoring system.
+#
+# === Parameters
+#
+# Coming soon...
+#
+# === Examples
+# 
+# Coming soon...
+# 
+
+class () icinga2::server inherits icinga2::params {
+  
+}
---
+++ manifests/server/install.pp
@@ -0,0 +1,18 @@
+# == Class: icinga2::server::install
+#
+# This class installs the server components for the Icinga 2 monitoring system.
+#
+# === Parameters
+#
+# Coming soon...
+#
+# === Examples
+# 
+# Coming soon...
+# 
+
+class icinga2::server::install inherits icinga2::server {
+  
+  include icinga2::params
+  
+}
