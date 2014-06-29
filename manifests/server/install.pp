f7da4468b6ad50a25a8ac2febd619306f093abc9
Nick Chappell <nick@intronic.org>
2014-06-28 22:05:37 -0700

    Added lightweight anchor pattern to the icinga2::server::install class.

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -14,5 +14,14 @@
 class icinga2::server::install inherits icinga2::server {
   
   include icinga2::params
+  #Apply our classes in the right order. Use the squiggly arrows (~>) to ensure that the 
+  #class left is applied before the class on the right and that it also refreshes the 
+  #class on the right.
+  #
+  #Here, we're setting up the package repos first, then installing the packages:
+  class{'icinga2::server::install::repos':} ~> 
+  class{'icinga2::server::install::packages':} ~> 
+  class{'icinga2::server::install::execs':} -> 
+  Class['icinga2::server::install']
   
 }
