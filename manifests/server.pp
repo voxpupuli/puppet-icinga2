6b4d6972e8e6d603defa946aa931b24e9801f470
Nick Chappell <nick@intronic.org>
2014-06-28 22:13:00 -0700

    Added lightweight anchor pattern to the icinga2::server class.

--- manifests/server.pp
+++ manifests/server.pp
@@ -11,6 +11,15 @@
 # Coming soon...
 # 
 
-class () icinga2::server inherits icinga2::params {
+class icinga2::server (
+  $server_db_type = $icinga2::params::server_db_type
+) inherits icinga2::params {
+
+  #Apply our classes in the right order. Use the squiggly arrows (~>) to ensure that the 
+  #class left is applied before the class on the right and that it also refreshes the 
+  #class on the right.
+  class {'icinga2::server::install':} #~>
+  #class {'icinga2::server::config':} ~>
+  #class {'icinga2::server::service':}
   
 }
