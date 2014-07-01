9a71775038d272eeb2cbd2a85b78da2b403530ac
Nick Chappell <nick@intronic.org>
2014-06-30 21:46:32 -0700

    Added logic to select the right DB schema file to load on Debian/Ubuntu.

--- manifests/params.pp
+++ manifests/params.pp
@@ -43,7 +43,11 @@ class icinga2::params {
       #Icinga 2 server package
       $icinga2_server_package = 'icinga2'
       
-      
+      #Pick set the right path where we can find the DB schema
+      case $server_db_type {
+        'mysql': { $server_db_schema_path = '/usr/share/doc/icinga2-ido-mysql-2.0.0/schema/mysql.sql' }
+        'pgsql': { $server_db_schema_path = '/usr/share/doc/icinga2-ido-pgsql-2.0.0/schema/pgsql.sql' }
+      }
     }
     
     #Fail if we're on any other OS:
