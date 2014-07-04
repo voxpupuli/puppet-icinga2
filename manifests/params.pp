d750ebd738686a01f834f32c1e79d27e77bf2924
Nick Chappell <nick@intronic.org>
2014-07-03 20:35:52 -0700

    Moved the logic to determine the path of the DB schema to the main icinga2::server class.

--- manifests/params.pp
+++ manifests/params.pp
@@ -34,12 +34,6 @@ class icinga2::params {
       $package_provider = 'yum'
       #Icinga 2 server package
       $icinga2_server_package = 'icinga2'
-      
-      #Pick set the right path where we can find the DB schema
-      case $server_db_type {
-        'mysql': { $server_db_schema_path = '/usr/share/doc/icinga2-ido-mysql-2.0.0/schema/mysql.sql' }
-        'pgsql': { $server_db_schema_path = '/usr/share/doc/icinga2-ido-pgsql-2.0.0/schema/pgsql.sql' }
-      }
     }
     
     #Debian/Ubuntu systems: 
@@ -48,12 +42,6 @@ class icinga2::params {
       $package_provider = 'apt'
       #Icinga 2 server package
       $icinga2_server_package = 'icinga2'
-      
-      #Pick set the right path where we can find the DB schema
-      case $server_db_type {
-        'mysql': { $server_db_schema_path = '/usr/share/icinga2-ido-mysql/schema/mysql.sql' }
-        'pgsql': { $server_db_schema_path = '/usr/share/icinga2-ido-pgsql/schema/pgsql.sql' }
-      }
     }
     
     #Fail if we're on any other OS:
