5a6e78d47d2f4d047ee6b54427add0be2b391ecc
Nick Chappell <nick@intronic.org>
2014-07-05 15:36:56 -0700

    Added a file resource for /etc/icinga2/icinga2.conf.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -14,6 +14,7 @@
 class icinga2::server::config inherits icinga2::server {
   
   include icinga2::params
+  
   #Directory resource for /etc/icinga2/
   file { '/etc/icinga2/':
     path    => '/etc/icinga2/',
@@ -24,6 +25,14 @@ class icinga2::server::config inherits icinga2::server {
     #require => Package[$icinga2::params::icinga2_server_packages],
   }
 
-
+  #File resource for /etc/icinga2/icinga2.conf:
+  file { '/etc/icinga2/icinga2.conf':
+    path    => '/etc/icinga2/icinga2.conf',
+    ensure  => file,
+    owner   => $etc_icinga2_icinga2_conf_owner,
+    group   => $etc_icinga2_icinga2_conf_group,
+    mode    => $etc_icinga2_icinga2_conf_mode,
+    content => template('icinga2/icinga2.conf.erb'),
+  }
   
 }
