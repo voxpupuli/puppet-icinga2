82a3cf983c715a935d6596837698dd2d204a7175
Nick Chappell <nick@intronic.org>
2014-07-05 15:24:13 -0700

    Added a directory resource for /etc/icinga2/.

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -14,5 +14,16 @@
 class icinga2::server::config inherits icinga2::server {
   
   include icinga2::params
+  #Directory resource for /etc/icinga2/
+  file { '/etc/icinga2/':
+    path    => '/etc/icinga2/',
+    ensure  => directory,
+    owner   => $etc_icinga2_owner,
+    group   => $etc_icinga2_group,
+    mode    => $etc_icinga2_mode,
+    #require => Package[$icinga2::params::icinga2_server_packages],
+  }
+
+
   
 }
