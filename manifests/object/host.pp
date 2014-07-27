6981ccf12d11cc9e6aa24cc0b29f6b39e2288007
Nick Chappell <nick@intronic.org>
2014-07-26 20:53:07 -0700

    Added an enable_flap_detection parameter and redering of it in the host object ERB template; removed it from the lists of needed parameters.

--- manifests/object/host.pp
+++ manifests/object/host.pp
@@ -24,9 +24,9 @@ define icinga2::object::host (
   $enable_active_checks = undef,
   $enable_passive_checks = undef,
   $enable_event_handler = undef,
+  $enable_flap_detection = undef,
 
   # Parameters that still need to be included:
-  # enable_flap_detection
   # enable_perfdata
   # event_command
   # flapping_threshold
