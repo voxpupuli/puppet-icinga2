9ce8678b7b4358d6676e65e0aa078380f6191565
Nick Chappell <nick@intronic.org>
2014-07-28 18:49:09 -0700

    Added more header comments.

--- manifests/object/hostgroup.pp
+++ manifests/object/hostgroup.pp
@@ -1,5 +1,8 @@
 # == Defined type: icinga2::object::hostgroup
 #
+# This is a defined type for Icinga 2 hostgroup objects.
+# See the following Icinga 2 doc page for more info:
+# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#objecttype-hostgroup
 #
 # === Parameters
 #
