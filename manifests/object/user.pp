fe21de08f0e16e26a9dd0c0ab147acfc30f530e7
Nick Chappell <nick@intronic.org>
2014-08-24 18:14:50 -0700

    Remove duplicate enable_notifications parameter that was added.

--- manifests/object/user.pp
+++ manifests/object/user.pp
@@ -14,7 +14,6 @@ define icinga2::object::user (
   $display_name = $name,
   $email = undef,
   $pager = undef,
-  $enable_notifications = 'true',
   $vars = {},
   $groups = [],
   $enable_notifications = undef,
