529cbb380ae7713a420c6de9e159b213522ed6d6
Alessandro Franceschi <al@lab42.it>
2014-09-17 08:21:25 +0000

    Fix for wrong package name in Debian 7
    Signed-off-by: Nick Chappell <nick@intronic.org>
    
    Taken from: https://github.com/Icinga/puppet-icinga2/pull/9
    
    refs #7264, https://dev.icinga.org/issues/7264

--- manifests/params.pp
+++ manifests/params.pp
@@ -114,7 +114,7 @@ class icinga2::params {
         #Only tested on Debian7
         '7': {
           $icinga2_server_package = 'icinga2'
-          $icinga2_server_plugin_packages = ["nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra", "nagios-nrpe-plugin"]
+          $icinga2_server_plugin_packages = ["nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-contrib", "nagios-nrpe-plugin"]
           $icinga2_server_mail_package = 'mailutils'
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $server_plugin_package_install_options = '--no-install-recommends'
@@ -361,7 +361,7 @@ class icinga2::params {
     'Debian': {
       case $::operatingsystemmajrelease {
         '7': {
-          $icinga2_client_packages = ["nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-extra", "nagios-nrpe-plugin"]
+          $icinga2_client_packages = ["nagios-nrpe-server", "nagios-plugins", "nagios-plugins-basic", "nagios-plugins-standard", "nagios-snmp-plugins", "nagios-plugins-contrib", "nagios-nrpe-plugin"]
           #Specify '--no-install-recommends' so we don't inadvertently get Nagios 3 installed; it comes as a recommended package with most of the plugin packages:
           $client_plugin_package_install_options = '--no-install-recommends'
         }
