5896e2b72a6be21445b603028aec4ce0652e6108
Steven Bambling <smbambling@gmail.com>
2014-09-26 11:57:42 -0700

    style guide fixes
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/object/service.pp
+++ manifests/object/service.pp
@@ -57,12 +57,12 @@ define icinga2::object::service (
   validate_string($target_file_mode)
 
   file {"${target_dir}/${target_file_name}":
-    ensure => file,
+    ensure  => file,
     owner   => $target_file_owner,
     group   => $target_file_group,
     mode    => $target_file_mode,
     content => template('icinga2/object_service.conf.erb'),
-    notify => Service['icinga2'],
+    notify  => Service['icinga2'],
   }
 
 }
