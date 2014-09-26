6ad22d4f966d8a3c12a2cf8c8b543746da48affd
Steven Bambling <smbambling@gmail.com>
2014-09-26 12:18:00 -0700

    sytle guide fixes
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/server/config.pp
+++ manifests/server/config.pp
@@ -17,8 +17,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/:
   file { '/etc/icinga2/':
-    path    => '/etc/icinga2/',
     ensure  => directory,
+    path    => '/etc/icinga2/',
     owner   => $etc_icinga2_owner,
     group   => $etc_icinga2_group,
     mode    => $etc_icinga2_mode,
@@ -27,8 +27,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #File resource for /etc/icinga2/icinga2.conf:
   file { '/etc/icinga2/icinga2.conf':
-    path    => '/etc/icinga2/icinga2.conf',
     ensure  => file,
+    path    => '/etc/icinga2/icinga2.conf',
     owner   => $etc_icinga2_icinga2_conf_owner,
     group   => $etc_icinga2_icinga2_conf_group,
     mode    => $etc_icinga2_icinga2_conf_mode,
@@ -37,8 +37,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/conf.d/:
   file { '/etc/icinga2/conf.d/':
-    path    => '/etc/icinga2/conf.d/',
     ensure  => directory,
+    path    => '/etc/icinga2/conf.d/',
     owner   => $etc_icinga2_confd_owner,
     group   => $etc_icinga2_confd_group,
     mode    => $etc_icinga2_confd_mode,
@@ -46,8 +46,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/features-available/:
   file { '/etc/icinga2/features-available/':
-    path    => '/etc/icinga2/features-available/',
     ensure  => directory,
+    path    => '/etc/icinga2/features-available/',
     owner   => $etc_icinga2_features_available_owner,
     group   => $etc_icinga2_features_available_group,
     mode    => $etc_icinga2_features_available_mode,
@@ -55,8 +55,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/features-enabled/:
   file { '/etc/icinga2/features-enabled/':
-    path    => '/etc/icinga2/features-enabled/',
     ensure  => directory,
+    path    => '/etc/icinga2/features-enabled/',
     owner   => $etc_icinga2_features_enabled_owner,
     group   => $etc_icinga2_features_enabled_group,
     mode    => $etc_icinga2_features_enabled_mode,
@@ -64,8 +64,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/pki/:
   file { '/etc/icinga2/pki/':
-    path    => '/etc/icinga2/pki/',
     ensure  => directory,
+    path    => '/etc/icinga2/pki/',
     owner   => $etc_icinga2_pki_owner,
     group   => $etc_icinga2_pki_group,
     mode    => $etc_icinga2_pki_mode,
@@ -73,8 +73,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/scripts/:
   file { '/etc/icinga2/scripts/':
-    path    => '/etc/icinga2/scripts/',
     ensure  => directory,
+    path    => '/etc/icinga2/scripts/',
     owner   => $etc_icinga2_scripts_owner,
     group   => $etc_icinga2_scripts_group,
     mode    => $etc_icinga2_scripts_mode,
@@ -82,8 +82,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/zones.d/:
   file { '/etc/icinga2/zones.d/':
-    path    => '/etc/icinga2/zones.d/',
     ensure  => directory,
+    path    => '/etc/icinga2/zones.d/',
     owner   => $etc_icinga2_zonesd_owner,
     group   => $etc_icinga2_zonesd_group,
     mode    => $etc_icinga2_zonesd_mode,
@@ -94,8 +94,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/:
   file { '/etc/icinga2/objects/':
-    path    => '/etc/icinga2/objects/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/',
     owner   => $etc_icinga2_obejcts_owner,
     group   => $etc_icinga2_obejcts_group,
     mode    => $etc_icinga2_obejcts_mode,
@@ -103,8 +103,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/hosts/:
   file { '/etc/icinga2/objects/hosts/':
-    path    => '/etc/icinga2/objects/hosts/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/hosts/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -112,8 +112,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/hostgroups/:
   file { '/etc/icinga2/objects/hostgroups/':
-    path    => '/etc/icinga2/objects/hostgroups/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/hostgroups/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -121,8 +121,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/services/:
   file { '/etc/icinga2/objects/services/':
-    path    => '/etc/icinga2/objects/services/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/services/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -130,8 +130,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/servicegroups/:
   file { '/etc/icinga2/objects/servicegroups/':
-    path    => '/etc/icinga2/objects/servicegroups/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/servicegroups/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -139,8 +139,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/users/:
   file { '/etc/icinga2/objects/users/':
-    path    => '/etc/icinga2/objects/users/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/users/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -148,8 +148,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/usergroups/:
   file { '/etc/icinga2/objects/usergroups/':
-    path    => '/etc/icinga2/objects/usergroups/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/usergroups/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -157,8 +157,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/checkcommands/:
   file { '/etc/icinga2/objects/checkcommands/':
-    path    => '/etc/icinga2/objects/checkcommands/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/checkcommands/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -166,8 +166,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/notificationcommands/:
   file { '/etc/icinga2/objects/notificationcommands/':
-    path    => '/etc/icinga2/objects/notificationcommands/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/notificationcommands/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -175,8 +175,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/eventcommands/:
   file { '/etc/icinga2/objects/eventcommands/':
-    path    => '/etc/icinga2/objects/eventcommands/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/eventcommands/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -184,8 +184,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/notifications/:
   file { '/etc/icinga2/objects/notifications/':
-    path    => '/etc/icinga2/objects/notifications/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/notifications/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -193,8 +193,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/timeperiods/:
   file { '/etc/icinga2/objects/timeperiods/':
-    path    => '/etc/icinga2/objects/timeperiods/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/timeperiods/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -202,8 +202,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/scheduleddowntimes/:
   file { '/etc/icinga2/objects/scheduleddowntimes/':
-    path    => '/etc/icinga2/objects/scheduleddowntimes/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/scheduleddowntimes/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -211,8 +211,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/dependencies/:
   file { '/etc/icinga2/objects/dependencies/':
-    path    => '/etc/icinga2/objects/dependencies/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/dependencies/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -220,8 +220,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/perfdatawriters/:
   file { '/etc/icinga2/objects/perfdatawriters/':
-    path    => '/etc/icinga2/objects/perfdatawriters/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/perfdatawriters/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -229,8 +229,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/graphitewriters/:
   file { '/etc/icinga2/objects/graphitewriters/':
-    path    => '/etc/icinga2/objects/graphitewriters/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/graphitewriters/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -238,8 +238,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/idomysqlconnections/:
   file { '/etc/icinga2/objects/idomysqlconnections/':
-    path    => '/etc/icinga2/objects/idomysqlconnections/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/idomysqlconnections/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -247,8 +247,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/idopgsqlconnections/:
   file { '/etc/icinga2/objects/idopgsqlconnections/':
-    path    => '/etc/icinga2/objects/idopgsqlconnections/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/idopgsqlconnections/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -256,8 +256,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/livestatuslisteners/:
   file { '/etc/icinga2/objects/livestatuslisteners/':
-    path    => '/etc/icinga2/objects/livestatuslisteners/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/livestatuslisteners/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -265,8 +265,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/statusdatawriters/:
   file { '/etc/icinga2/objects/statusdatawriters/':
-    path    => '/etc/icinga2/objects/statusdatawriters/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/statusdatawriters/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -274,8 +274,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/externalcommandlisteners/:
   file { '/etc/icinga2/objects/externalcommandlisteners/':
-    path    => '/etc/icinga2/objects/externalcommandlisteners/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/externalcommandlisteners/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -283,8 +283,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/compatloggers/:
   file { '/etc/icinga2/objects/compatloggers/':
-    path    => '/etc/icinga2/objects/compatloggers/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/compatloggers/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -292,8 +292,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/checkresultreaders/:
   file { '/etc/icinga2/objects/checkresultreaders/':
-    path    => '/etc/icinga2/objects/checkresultreaders/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/checkresultreaders/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -301,8 +301,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/checkercomponents/:
   file { '/etc/icinga2/objects/checkercomponents/':
-    path    => '/etc/icinga2/objects/checkercomponents/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/checkercomponents/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -310,8 +310,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/notificationcomponents/:
   file { '/etc/icinga2/objects/notificationcomponents/':
-    path    => '/etc/icinga2/objects/notificationcomponents/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/notificationcomponents/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -319,8 +319,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/fileloggers/:
   file { '/etc/icinga2/objects/fileloggers/':
-    path    => '/etc/icinga2/objects/fileloggers/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/fileloggers/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -328,8 +328,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/syslogloggers/:
   file { '/etc/icinga2/objects/syslogloggers/':
-    path    => '/etc/icinga2/objects/syslogloggers/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/syslogloggers/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -337,8 +337,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/icingastatuswriters/:
   file { '/etc/icinga2/objects/icingastatuswriters/':
-    path    => '/etc/icinga2/objects/icingastatuswriters/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/icingastatuswriters/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -346,8 +346,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/apilisteners/:
   file { '/etc/icinga2/objects/apilisteners/':
-    path    => '/etc/icinga2/objects/apilisteners/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/apilisteners/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -355,8 +355,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/endpoints/:
   file { '/etc/icinga2/objects/endpoints/':
-    path    => '/etc/icinga2/objects/endpoints/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/endpoints/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -364,8 +364,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/zones/:
   file { '/etc/icinga2/objects/zones/':
-    path    => '/etc/icinga2/objects/zones/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/zones/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -377,8 +377,8 @@ class icinga2::server::config inherits icinga2::server {
   #See the following link for more info:
   # http://docs.icinga.org/icinga2/latest/doc/module/icinga2/toc#!/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#apply
   file { '/etc/icinga2/objects/applys/':
-    path    => '/etc/icinga2/objects/applys/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/applys/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -386,8 +386,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/templates/:
   file { '/etc/icinga2/objects/templates/':
-    path    => '/etc/icinga2/objects/templates/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/templates/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
@@ -396,8 +396,8 @@ class icinga2::server::config inherits icinga2::server {
 
   #Directory resource for /etc/icinga2/objects/constants/:
   file { '/etc/icinga2/objects/constants/':
-    path    => '/etc/icinga2/objects/constants/',
     ensure  => directory,
+    path    => '/etc/icinga2/objects/constants/',
     owner   => $etc_icinga2_obejcts_sub_dir_owner,
     group   => $etc_icinga2_obejcts_sub_dir_group,
     mode    => $etc_icinga2_obejcts_sub_dir_mode,
