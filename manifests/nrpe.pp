05cae20306f99bbcb2a1d41836560c07b3177ade
Steven Bambling <smbambling@gmail.com>
2014-09-26 11:43:13 -0700

    style guide fixes
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/nrpe.pp
+++ manifests/nrpe.pp
@@ -19,8 +19,8 @@ class icinga2::nrpe (
   validate_string($nrpe_log_facility)
   validate_array($nrpe_allowed_hosts)
 
-  #Apply our classes in the right order. Use the squiggly arrows (~>) to ensure that the 
-  #class left is applied before the class on the right and that it also refreshes the 
+  #Apply our classes in the right order. Use the squiggly arrows (~>) to ensure that the
+  #class left is applied before the class on the right and that it also refreshes the
   #class on the right.
   class {'icinga2::nrpe::install':} ~>
   class {'icinga2::nrpe::config':} ~>
