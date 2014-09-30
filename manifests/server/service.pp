664d918edf87f9c9b856ef1800f641209555ff8c
Steven Bambling <smbambling@gmail.com>
2014-09-30 17:25:59 -0400

    making the server watch for enabled features/modules
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/server/service.pp
+++ manifests/server/service.pp
@@ -18,7 +18,7 @@ class icinga2::server::service inherits icinga2::server {
   #Service resource for the Icinga 2 daemon:
   service {$icinga2::params::icinga2_server_service_name:
     ensure    => running,
-    subscribe => Class['icinga2::server::config'],
+    subscribe => [ Class['icinga2::server::config'], Class['icinga2::server::features'] ],
   }
 
 }
