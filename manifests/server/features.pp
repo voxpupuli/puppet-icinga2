6526467f42b4b2c3adfdff34d65f440925507a78
Steven Bambling <smbambling@gmail.com>
2014-10-02 07:00:43 -0400

    calling disable define and adding new params
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/server/features.pp
+++ manifests/server/features.pp
@@ -1,12 +1,17 @@
 # Enable Features for Icinga 2
 class icinga2::server::features (
   $enabled_features = $icinga2::server::server_enabled_features,
+  $disabled_features = $icinga2::server::server_disabled_features,
 ) {
 
   # Do some checking
   validate_array($enabled_features)
+  validate_array($disabled_features)
 
-  # Pass the features arry to the define to loop though
+  #Pass the disabled features array to the define for looping
+  icinga2::server::features::disable { $disabled_features: }
+
+  #Pass the features array to the define for looping
   icinga2::server::features::enable { $enabled_features: }
 
 }
