01e34302429bdcc23fd0cc1f2152074279391272
Steven Bambling <smbambling@gmail.com>
2014-10-02 08:26:46 -0400

    fixing missing , after parameters
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/server.pp
+++ manifests/server.pp
@@ -24,8 +24,8 @@ class icinga2::server (
   $icinga2_server_package = $icinga2::params::icinga2_server_package,
   $server_install_nagios_plugins = $icinga2::params::server_install_nagios_plugins,
   $install_mail_utils_package = $icinga2::params::install_mail_utils_package,
-  $server_enabled_features = $icinga2::params::server_enabled_features
-  $server_disabled_features = $icinga2::params::server_disabled_features
+  $server_enabled_features = $icinga2::params::server_enabled_features,
+  $server_disabled_features = $icinga2::params::server_disabled_features,
 ) inherits icinga2::params {
 
   #Do some validation of parameters so we know we have the right data types:
