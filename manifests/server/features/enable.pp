4fdc002c9052ea2602aefb5fe21b37dc9593aad6
Steven Bambling <smbambling@gmail.com>
2014-10-02 08:06:53 -0400

    fixing define name
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/server/features/enable.pp
+++ manifests/server/features/enable.pp
@@ -1,5 +1,5 @@
 # Enable Icinga 2 Features/Modules
-define icinga2::server::feature::enable () {
+define icinga2::server::features::enable () {
   exec { "icinga2-enable-feature ${name}":
     user    => 'root',
     path    => '/usr/bin:/usr/sbin:/bin/:/sbin',
