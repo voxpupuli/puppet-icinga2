075ca2a4bfe9e742243fe11f78799b9c0965ce6f
Steven Bambling <smbambling@gmail.com>
2014-10-03 06:38:14 -0400

    adding new parameter for checkplugin_libdir to keep backwards compatibility
    Signed-off-by: Nick Chappell <nick@intronic.org>

--- manifests/params.pp
+++ manifests/params.pp
@@ -298,6 +298,7 @@ class icinga2::params {
     'CentOS': {
       $nrpe_config_basedir = '/etc/nagios'
       $nrpe_plugin_libdir  = '/usr/lib64/nagios/plugins'
+      $checkplugin_libdir   = '/usr/lib/nagios/plugins'
       $nrpe_pid_file_path  = '/var/run/nrpe/nrpe.pid'
       $nrpe_user           = 'nrpe'
       $nrpe_group          = 'nrpe'
@@ -306,6 +307,7 @@ class icinga2::params {
     'Ubuntu': {
       $nrpe_config_basedir  = '/etc/nagios'
       $nrpe_plugin_libdir   = '/usr/lib/nagios/plugins'
+      $checkplugin_libdir   = '/usr/lib/nagios/plugins'
       $nrpe_pid_file_path   = '/var/run/nagios/nrpe.pid'
       $nrpe_user            = 'nagios'
       $nrpe_group           = 'nagios'
@@ -314,6 +316,7 @@ class icinga2::params {
     'Debian': {
       $nrpe_config_basedir  = '/etc/nagios'
       $nrpe_plugin_libdir   = '/usr/lib/nagios/plugins'
+      $checkplugin_libdir   = '/usr/lib/nagios/plugins'
       $nrpe_pid_file_path   = '/var/run/nagios/nrpe.pid'
       $nrpe_user            = 'nagios'
       $nrpe_group           = 'nagios'
