28961d8fce207ef08a593a6fc2b72936a21822a2
Nick Chappell <nick@intronic.org>
2014-10-09 23:43:02 -0700

    Comments in the icinga2::object::graphitewriter manifest.

--- manifests/object/graphitewriter.pp
+++ manifests/object/graphitewriter.pp
@@ -1,7 +1,19 @@
-# Configure GraphiteWriter Feature .conf file
+# == Defined type: icinga2::object::graphitewriter
+#
+#  This is a defined type for Icinga 2 GraphiteWriter objects.
+# See the following Icinga 2 doc page for more info:
+# http://docs.icinga.org/icinga2/latest/doc/module/icinga2/chapter/configuring-icinga2#objecttype-graphitewriter
+#
+# === Parameters
+#
+# See the inline comments.
+#
+
 define icinga2::object::graphitewriter (
   $graphite_host        = '127.0.0.1',
   $graphite_port        = 2003,
+  #Put the object files this defined type generates in features-available
+  #since the Graphite writer feature is one that has to be explicitly enabled.
   $target_dir           = '/etc/icinga2/features-available',
   $target_file_name     = "${name}.conf",
   $target_file_owner    = 'root',
