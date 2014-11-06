4c3c60335033f624bdf828a91e55e282b79c3217
Tom De Vylder <tom@penumbra.be>
2014-11-06 14:39:20 +0100

    Purge unmanaged files from nrpe.d
    Signed-off-by: Nick Chappell <nick@intronic.org>
    
    Merged from: https://github.com/Icinga/puppet-icinga2/pull/39
    
    refs#7673: https://dev.icinga.org/issues/7673

--- manifests/nrpe/config.pp
+++ manifests/nrpe/config.pp
@@ -24,6 +24,8 @@ class icinga2::nrpe::config inherits icinga2::nrpe {
     owner   => 'root',
     group   => 'root',
     mode    => '0755',
+    purge   => true,
+    recurse => true,
     require => Package[$icinga2::params::icinga2_client_packages],
   }
 
