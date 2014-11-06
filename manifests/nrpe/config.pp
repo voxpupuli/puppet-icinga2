48d5bfb0ce66e7363cc8985682a9c2c4801f054e
Tom De Vylder <tom@penumbra.be>
2014-11-06 23:10:09 +0100

    Add previously unmanaged directory
    Signed-off-by: Nick Chappell <nick@intronic.org>
    
    Merged from: https://github.com/Icinga/puppet-icinga2/pull/39
    
    refs#7673: https://dev.icinga.org/issues/7673

--- manifests/nrpe/config.pp
+++ manifests/nrpe/config.pp
@@ -29,6 +29,16 @@ class icinga2::nrpe::config inherits icinga2::nrpe {
     require => Package[$icinga2::params::icinga2_client_packages],
   }
 
+  file { '/etc/nrpe.d':
+    ensure  => directory,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    purge   => $nrpe_purge_unmanaged,
+    recurse => true,
+    require => Package[$icinga2::params::icinga2_client_packages],
+  }
+
   #File resource for /etc/nagios/nrpe.cfg
   file { '/etc/nagios/nrpe.cfg':
     ensure  => file,
