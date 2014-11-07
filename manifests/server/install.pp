f74aab291b9a544ec9cb8119e3c18f29797f3506
Josh Holland <josh@inv.alid.pw>
2014-11-07 16:25:23 +0000

    Further fix the quoting of PostgreSQL password
    Signed-off-by: Nick Chappell <nick@intronic.org>
    
    Merged from: https://github.com/Icinga/puppet-icinga2/pull/40
    
    refs#7675: https://dev.icinga.org/issues/7675

--- manifests/server/install.pp
+++ manifests/server/install.pp
@@ -160,7 +160,7 @@ class icinga2::server::install::execs inherits icinga2::server {
       exec { 'postgres_schema_load':
         user    => 'root',
         path    => '/usr/bin:/usr/sbin:/bin/:/sbin',
-        command => "su - postgres -c 'export PGPASSWORD=''${db_password}'' && psql -U ${db_user} -h localhost -d ${db_name} < ${server_db_schema_path}' && export PGPASSWORD='' && touch /etc/icinga2/postgres_schema_loaded.txt",
+        command => "su - postgres -c 'export PGPASSWORD='\''${db_password}'\'' && psql -U ${db_user} -h localhost -d ${db_name} < ${server_db_schema_path}' && export PGPASSWORD='' && touch /etc/icinga2/postgres_schema_loaded.txt",
         creates => '/etc/icinga2/postgres_schema_loaded.txt',
         require => Class['icinga2::server::install::packages'],
       }
