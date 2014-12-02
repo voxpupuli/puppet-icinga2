251184b944363a3464bc8fe9a950daab1f3bbf68
Nick Chappell <nick@intronic.org>
2014-12-01 21:38:42 -0800

    Added refresh_icinga2_service parameters to each of the object defined types so that they can be used by themselves if the module isn't being used to also manage the Icinga 2 service.
    refs# 7856 : https://dev.icinga.org/issues/7856

--- manifests/object/timeperiod.pp
+++ manifests/object/timeperiod.pp
@@ -15,12 +15,13 @@ define icinga2::object::timeperiod (
   $timeperiod_display_name       = undef,
   $methods                       = undef,
   $ranges                        = {},
-  $timeperiod_target_dir         = '/etc/icinga2/objects/timeperiods',
-  $timeperiod_target_file_name   = "${name}.conf",
+  $target_dir                    = '/etc/icinga2/objects/timeperiods',
+  $target_file_name              = "${name}.conf",
   $target_file_ensure            = file,
   $target_file_owner             = 'root',
   $target_file_group             = 'root',
   $target_file_mode              = '0644',
+  $refresh_icinga2_service = true
 ) {
 
   # Do some validation of the class' parameters:
@@ -36,13 +37,33 @@ define icinga2::object::timeperiod (
   validate_string($target_file_owner)
   validate_string($target_file_group)
   validate_re($target_file_mode, '^\d{4}$')
+  validate_bool($refresh_icinga2_service)
 
-  file {"${timeperiod_target_dir}/${timeperiod_target_file_name}":
-    ensure  => $target_file_ensure,
-    owner   => $target_file_owner,
-    group   => $target_file_group,
-    mode    => $target_file_mode,
-    content => template('icinga2/object_timeperiod.conf.erb'),
-    notify  => Service['icinga2'],
+  #If the refresh_icinga2_service parameter is set to true...
+  if $refresh_icinga2_service == true {
+
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_timeperiod.conf.erb'),
+      #...notify the Icinga 2 daemon so it can restart and pick up changes made to this config file...
+      notify  => Service['icinga2'],
+    }
+
   }
+  #...otherwise, use the same file resource but without a notify => parameter: 
+  else {
+  
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_timeperiod.conf.erb'),
+    }
+  
+  }
+
 }
