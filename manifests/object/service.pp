6c5497d3c8d8f829fa17ff65bc83b854983acddc
Nick Chappell <nick@intronic.org>
2014-12-01 21:38:42 -0800

    Added refresh_icinga2_service parameters to each of the object defined types so that they can be used by themselves if the module isn't being used to also manage the Icinga 2 service.
    refs# 7856 : https://dev.icinga.org/issues/7856

--- manifests/object/service.pp
+++ manifests/object/service.pp
@@ -41,7 +41,8 @@ define icinga2::object::service (
   $target_file_ensure = file,
   $target_file_owner  = 'root',
   $target_file_group  = 'root',
-  $target_file_mode   = '0644'
+  $target_file_mode   = '0644',
+  $refresh_icinga2_service = true
 ) {
 
   #Do some validation of the class' parameters:
@@ -56,14 +57,33 @@ define icinga2::object::service (
   validate_string($target_file_owner)
   validate_string($target_file_group)
   validate_string($target_file_mode)
+  validate_bool($refresh_icinga2_service)
 
-  file {"${target_dir}/${target_file_name}":
-    ensure  => $target_file_ensure,
-    owner   => $target_file_owner,
-    group   => $target_file_group,
-    mode    => $target_file_mode,
-    content => template('icinga2/object_service.conf.erb'),
-    notify  => Service['icinga2'],
+  #If the refresh_icinga2_service parameter is set to true...
+  if $refresh_icinga2_service == true {
+
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_service.conf.erb'),
+      #...notify the Icinga 2 daemon so it can restart and pick up changes made to this config file...
+      notify  => Service['icinga2'],
+    }
+
+  }
+  #...otherwise, use the same file resource but without a notify => parameter: 
+  else {
+  
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_service.conf.erb'),
+    }
+  
   }
 
 }
--- manifests/object/user.pp
+++ manifests/object/user.pp
@@ -25,7 +25,8 @@ define icinga2::object::user (
   $target_file_ensure = file, 
   $target_file_owner = 'root',
   $target_file_group = 'root',
-  $target_file_mode = '0644'
+  $target_file_mode = '0644',
+  $refresh_icinga2_service = true
 ) {
 
   #Do some validation of the class' parameters:
@@ -41,14 +42,33 @@ define icinga2::object::user (
   validate_string($target_file_owner)
   validate_string($target_file_group)
   validate_string($target_file_mode)
+  validate_bool($refresh_icinga2_service)
 
-  file {"${target_dir}/${target_file_name}":
-    ensure  => $target_file_ensure,
-    owner   => $target_file_owner,
-    group   => $target_file_group,
-    mode    => $target_file_mode,
-    content => template('icinga2/object_user.conf.erb'),
-    notify  => Service['icinga2'],
+  #If the refresh_icinga2_service parameter is set to true...
+  if $refresh_icinga2_service == true {
+
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_user.conf.erb'),
+      #...notify the Icinga 2 daemon so it can restart and pick up changes made to this config file...
+      notify  => Service['icinga2'],
+    }
+
+  }
+  #...otherwise, use the same file resource but without a notify => parameter: 
+  else {
+  
+    file { "${target_dir}/${target_file_name}":
+      ensure  => $target_file_ensure,
+      owner   => $target_file_owner,
+      group   => $target_file_group,
+      mode    => $target_file_mode,
+      content => template('icinga2/object_user.conf.erb'),
+    }
+  
   }
 
 }
