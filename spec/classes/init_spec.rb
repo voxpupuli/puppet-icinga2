b9e14304f33c5cabb534d92495f42e5a45281a7a
Blerim Sheqa <blerim.sheqa@netways.de>
2016-09-08 16:46:57 +0200

    update unit tests for icinga::

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -2,6 +2,14 @@ require 'spec_helper'
 
 describe('icinga2', :type => :class) do
 
+  before(:all) do
+    @icinga2_conf = "/etc/icinga2/icinga2.conf"
+    @constants_conf = "/etc/icinga2/constants.conf"
+
+    @windows_icinga2_conf = "C:/ProgramData/icinga2/etc/icinga2/icinga2.conf"
+    @windows_constants_conf = "C:/ProgramData/icinga2/etc/icinga2/constants.conf"
+  end
+
   on_supported_os.each do |os, facts|
     let :facts do
       facts
@@ -16,20 +24,43 @@ describe('icinga2', :type => :class) do
         })
       }
 
-      it { is_expected.to contain_file('/etc/icinga2/constants.conf').with_content %r{^const PluginDir = \"/usr/lib/nagios/plugins\"\n} }
-      it { is_expected.to contain_file('/etc/icinga2/constants.conf').with_content %r{^const PluginContribDir = \"/usr/lib/nagios/plugins\"\n} }
-      it { is_expected.to contain_file('/etc/icinga2/constants.conf').with_content %r{^const ManubulonPluginDir = \"/usr/lib/nagios/plugins\"\n} }
-      it { is_expected.to contain_file('/etc/icinga2/constants.conf').with_content %r{^const NodeName = \".+\"\n} }
-      it { is_expected.to contain_file('/etc/icinga2/constants.conf').with_content %r{^const TicketSalt = \"\"\n} }
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const PluginDir = \"/usr/lib/nagios/plugins\"\n} }
 
-      it { is_expected.to contain_file('/etc/icinga2/icinga2.conf').with_content %r{^// managed by puppet\n} }
-      it { is_expected.to contain_file('/etc/icinga2/icinga2.conf').with_content %r{^include <plugins>\n} }
-      it { is_expected.to contain_file('/etc/icinga2/icinga2.conf').with_content %r{^include <plugins-contrib>\n} }
-      it { is_expected.to contain_file('/etc/icinga2/icinga2.conf').with_content %r{^include_recursive \"conf.d\"\n} }
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const PluginContribDir = \"/usr/lib/nagios/plugins\"\n} }
 
-      it { should_not contain_yumrepo('icinga-stable-release') }
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const ManubulonPluginDir = \"/usr/lib/nagios/plugins\"\n} }
+
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const NodeName = \".+\"\n} }
+
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const ZoneName = \".+\"\n} }
+
+      it { is_expected.to contain_file(@constants_conf)
+        .with_content %r{^const TicketSalt = \"\"\n} }
+
+      it { is_expected.to contain_file(@icinga2_conf)
+        .with_content %r{^// managed by puppet\n} }
+
+      it { is_expected.to contain_file(@icinga2_conf)
+        .with_content %r{^include <plugins>\n} }
+
+      it { is_expected.to contain_file(@icinga2_conf)
+        .with_content %r{^include <plugins-contrib>\n} }
+
+      it { is_expected.to contain_file(@icinga2_conf)
+        .with_content %r{^include_recursive \"conf.d\"\n} }
+
+      case facts[:osfamily]
+      when 'Debian'
+        it { should_not contain_apt__source('icinga-stable-release') } 
+      when 'RedHat'
+        it { should_not contain_yumrepo('icinga-stable-release') }
+      end
     end
-
   end
 
   context 'Windows 2012 R2 with all default parameters' do
@@ -49,34 +80,49 @@ describe('icinga2', :type => :class) do
       })
     }
 
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/constants.conf')
+
+
+    it { is_expected.to contain_file(@windows_constants_conf)
       .with_content %r{^const PluginDir = \"C:/Program Files/ICINGA2/sbin\"\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/constants.conf')
+
+    it { is_expected.to contain_file(@windows_constants_conf)
       .with_content %r{^const PluginContribDir = \"C:/Program Files/ICINGA2/sbin\"\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/constants.conf')
+
+    it { is_expected.to contain_file(@windows_constants_conf)
       .with_content %r{^const ManubulonPluginDir = \"C:/Program Files/ICINGA2/sbin\"\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/constants.conf')
+
+    it { is_expected.to contain_file(@windows_constants_conf)
+      .with_content %r{^const NodeName = \".+\"\r\n} }
+
+    it { is_expected.to contain_file(@windows_constants_conf)
+      .with_content %r{^const ZoneName = \".+\"\r\n} }
+
+    it { is_expected.to contain_file(@windows_constants_conf)
       .with_content %r{^const TicketSalt = \"\"\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/constants.conf')
+
+    it { is_expected.to contain_file(@windows_constants_conf)
       .with_content %r{^} }
 
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/icinga2.conf').with_content %r{^// managed by puppet\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/iicnga2.conf').with_content %r{^include <windows-plugins>\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/icinga2.conf').with_content %r{^include <nscp>\r\n} }
-    it { is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/icinga2.conf').with_content %r{^include_recursive \"conf.d\"\r\n} }
+    it { is_expected.to contain_file(@windows_icinga2_conf)
+      .with_content %r{^// managed by puppet\r\n} }
 
-    it { should_not contain_yumrepo('icinga-stable-release') }
+    it { is_expected.to contain_file(@windows_icinga2_conf)
+      .with_content %r{^include <windows-plugins>\r\n} }
 
+    it { is_expected.to contain_file(@windows_icinga2_conf)
+      .with_content %r{^include <nscp>\r\n} }
 
+    it { is_expected.to contain_file(@windows_icinga2_conf)
+      .with_content %r{^include_recursive \"conf.d\"\r\n} }
   end
 
-#  context 'on unsupported plattform' do
-#    let(:facts) { {:osfamily => 'foo'} }
-#    it do
-#      expect {
-#        should contain_class('icinga')
-#      }.to raise_error(Puppet::Error, /foo is not supported/)
-#    end
-#  end
+  context 'on unsupported plattform' do
+    let(:facts) { {:osfamily => 'foo'} }
+    it do
+      expect {
+        should contain_class('icinga')
+      }.to raise_error(Puppet::Error, /foo is not supported/)
+    end
+  end
 
 end
