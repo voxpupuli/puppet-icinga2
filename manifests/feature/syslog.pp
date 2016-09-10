907a5aa77961ace784bb6006dd865ea2d9ddb91b
Lennart Betz <lennart.betz@netways.de>
2016-09-10 18:01:17 +0000

    add feature syslog

---
+++ manifests/feature/syslog.pp
@@ -0,0 +1,33 @@
+# == Class: icinga2::feature::syslog
+#
+# This module configures the Icinga2 feature syslog.
+#
+# === Parameters
+#
+# Document parameters here.
+#
+# [*ensure*]
+#   Set to present enables the feature syslog, absent disabled it. Default to present.
+#
+# [*severity*]
+#   You can choose the log severity between information, notice, warning or debug.
+#   Default to information.
+#
+# === Authors
+#
+# Icinga Development Team <info@icinga.org>
+#
+class icinga2::feature::syslog(
+  $ensure   = present,
+  $severity = 'warning',
+) inherits icinga2::params {
+
+  validate_re($ensure, [ '^present$', '^absent$' ],
+    "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
+  validate_re($severity, ['^information$','^notice$','^warning$','^debug$'])
+
+  icinga2::feature { 'syslog':
+    ensure => $ensure,
+  }
+}
+
