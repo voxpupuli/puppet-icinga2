338ed8fe04e5ede3c69fb73e0334cadd1b8268dd
Lennart Betz <lennart.betz@netways.de>
2016-09-10 16:47:36 +0000

    rework unit tests

--- spec/classes/statusdata_spec.rb
+++ spec/classes/statusdata_spec.rb
@@ -1,107 +1,222 @@
 require 'spec_helper'
-require 'plattforms'
 
 describe('icinga2::feature::statusdata', :type => :class) do
-  # reference plattform for Linux
-  let(:facts) { IcingaPuppet.plattforms['RedHat 7'] }
   let(:pre_condition) { [
     "class { 'icinga2': features => [], }"
   ] }
 
-  context 'with ensure => present on all supported plattforms' do
+  on_supported_os.each do |os, facts|
+    let :facts do
+      facts
+    end
+
+    context "#{os} with ensure => present" do
+      let(:params) { {:ensure => 'present'} }
+
+      it { is_expected.to contain_icinga2__feature('statusdata').with({'ensure' => 'present'}) }
+    end
+
+
+    context "#{os} with ensure => absent" do
+      let(:params) { {:ensure => 'absent'} }
+
+      it { is_expected.to contain_icinga2__feature('statusdata').with({'ensure' => 'absent'}) }
+    end
+
+
+    context "#{os} with update_interval => 1m" do
+      let(:params) { {:update_interval => '1m'} }
+
+      it {
+        is_expected.to contain_file('/etc/icinga2/features-available/statusdata.conf')
+          .with_content(/update_interval = 1m/)
+      }
+    end
+
+
+    context "#{os} with update_interval => foo (not a valid value)" do
+      let(:params) { {:update_interval => 'foo'} }
+
+      it do
+        expect {
+          is_expected.to contain_icinga2__feature('statusdata')
+        }.to raise_error(Puppet::Error, /"foo" does not match/)
+      end
+    end
+
+
+    context "#{os} with status_path => /foo/bar" do
+      let(:params) { {:status_path => '/foo/bar'} }
+
+      it {
+        is_expected.to contain_file('/etc/icinga2/features-available/statusdata.conf')
+          .with_content(/status_path = "\/foo\/bar"/)
+      }
+    end
+
+
+    context "#{os} with status_path => foo/bar (not an absolute path)" do
+      let(:params) { {:status_path => 'foo/bar'} }
+
+      it do
+        expect {
+          is_expected.to contain_icinga2__feature('statusdata')
+        }.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/)
+      end
+    end
+
+
+    context "#{os} with objects_path => /foo/bar" do
+      let(:params) { {:objects_path => '/foo/bar'} }
+
+      it {
+        is_expected.to contain_file('/etc/icinga2/features-available/statusdata.conf')
+          .with_content(/objects_path = "\/foo\/bar"/)
+      }
+    end
+
+
+    context "#{os} with objects_path => foo/bar (not an absolute path)" do
+      let(:params) { {:objects_path => 'foo/bar'} }
+
+      it do
+        expect {
+          is_expected.to contain_icinga2__feature('statusdata')
+        }.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/)
+      end
+    end
+  end
+
+
+  context 'Windows 2012 R2 with ensure => present' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:ensure => 'present'} }
-    it do
-      should contain_icinga2__feature('statusdata').with({
-        'ensure' => 'present',
-      })
-    end
+
+    it { is_expected.to contain_icinga2__feature('statusdata').with({'ensure' => 'present'}) }
   end
 
-  context 'with enable => absent on  on all supported plattforms' do
+
+  context 'Windows 2012 R2 with ensure => absent' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:ensure => 'absent'} }
-    it do
-      should contain_icinga2__feature('statusdata').with({
-        'ensure' => 'absent',
-      })
-    end
+
+    it { is_expected.to contain_icinga2__feature('statusdata').with({'ensure' => 'absent'}) }
   end
 
-  context 'with update_interval => 1m on Linux' do
+
+  context 'Windows 2012 R2 with update_interval => 1m' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:update_interval => '1m'} }
-    it do
-      should contain_file('/etc/icinga2/features-available/statusdata.conf')
+
+    it {
+      is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
         .with_content(/update_interval = 1m/)
-    end
+    }
   end
 
-  context 'with update_interval => 60s on Windows' do
-    let(:facts) { IcingaPuppet.plattforms['Windows 2012 R2'] }
-    let(:params) { {:update_interval => '60s'} }
-    it do
-      should contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
-        .with_content(/update_interval = 60s/)
-    end
-  end
 
-  context 'with update_interval => foo (not a valid value)' do
+  context 'Windows 2012 R2 with update_interval => foo (not a valid value)' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:update_interval => 'foo'} }
+
     it do
       expect {
-        should contain_icinga2__feature('statusdata')
+        is_expected.to contain_icinga2__feature('statusdata')
       }.to raise_error(Puppet::Error, /"foo" does not match/)
     end
   end
 
-  context 'with status_path => /foo/bar on Linux' do
-    let(:params) { {:status_path => '/foo/bar'} }
-    it do
-      should contain_file('/etc/icinga2/features-available/statusdata.conf')
-        .with_content(/status_path = "\/foo\/bar"/)
-    end
+
+  context 'Windows 2012 R2 with status_path => c:/foo/bar' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:status_path => 'c:/foo/bar'} }
+
+    it {
+      is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
+        .with_content(/status_path = "c:\/foo\/bar"/)
+    }
   end
 
-  context 'with status_path => C:/foo/bar on Windows' do
-    let(:facts) { IcingaPuppet.plattforms['Windows 2012 R2'] }
-    let(:params) { {:status_path => 'C:/foo/bar'} }
-    it do
-      should contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
-        .with_content(/status_path = "C:\/foo\/bar"/)
-    end
-  end
 
-  context 'with status_path => foo/bar (not a valid value)' do
+  context 'Windows 2012 R2 with status_path => foo/bar (not an absolute path)' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:status_path => 'foo/bar'} }
+
     it do
       expect {
-        should contain_icinga2__feature('statusdata')
+        is_expected.to contain_icinga2__feature('statusdata')
       }.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/)
     end
   end
 
-  context 'with objects_path => /foo/bar on Linux' do
-    let(:params) { {:objects_path => '/foo/bar'} }
-    it do
-      should contain_file('/etc/icinga2/features-available/statusdata.conf')
-        .with_content(/objects_path = "\/foo\/bar"/)
-    end
+
+  context 'Windows 2012 R2 with objects_path => c:/foo/bar' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:objects_path => 'c:/foo/bar'} }
+
+    it {
+      is_expected.to contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
+        .with_content(/objects_path = "c:\/foo\/bar"/)
+    }
   end
 
-  context 'with objects_path => C:/foo/bar on Windows' do
-    let(:facts) { IcingaPuppet.plattforms['Windows 2012 R2'] }
-    let(:params) { {:objects_path => 'C:/foo/bar'} }
-    it do
-      should contain_file('C:/ProgramData/icinga2/etc/icinga2/features-available/statusdata.conf')
-        .with_content(/objects_path = "C:\/foo\/bar"/)
-    end
-  end
 
-  context 'with objects_path => foo/bar (not a valid value)' do
+  context 'Windows 2012 R2 with objects_path => foo/bar (not an absolute path)' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
     let(:params) { {:objects_path => 'foo/bar'} }
+
     it do
       expect {
-        should contain_icinga2__feature('statusdata')
+        is_expected.to contain_icinga2__feature('statusdata')
       }.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/)
     end
   end
-
 end
