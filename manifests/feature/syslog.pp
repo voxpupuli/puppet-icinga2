e4dc2ee179c7f0063fa18d191fd79edc2de3d62f
Lennart Betz <lennart.betz@netways.de>
2016-09-11 21:00:17 +0000

    remove params inherits from feature syslog

--- manifests/feature/syslog.pp
+++ manifests/feature/syslog.pp
@@ -20,7 +20,7 @@
 class icinga2::feature::syslog(
   $ensure   = present,
   $severity = 'warning',
-) inherits icinga2::params {
+) {
 
   validate_re($ensure, [ '^present$', '^absent$' ],
     "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
