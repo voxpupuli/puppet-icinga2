7f382ecc80e0694683aec9377fb2b29f1ece80d4
Lennart Betz <lennart.betz@netways.de>
2016-09-11 20:56:52 +0000

    remove params inherits from feature opentsdb

--- manifests/feature/opentsdb.pp
+++ manifests/feature/opentsdb.pp
@@ -23,7 +23,7 @@ class icinga2::feature::opentsdb(
   $ensure               = present,
   $host                 = '127.0.0.1',
   $port                 = '4242',
-) inherits icinga2::params {
+) {
 
   validate_re($ensure, [ '^present$', '^absent$' ],
     "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
