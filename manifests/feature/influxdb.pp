8ef6a50251a57143a200d42650bf5ed6cc736166
Lennart Betz <lennart.betz@netways.de>
2016-09-12 19:25:56 +0000

    correct hashes in feature influxdb

--- manifests/feature/influxdb.pp
+++ manifests/feature/influxdb.pp
@@ -8,49 +8,49 @@
 #   Set to present enables the feature graphite, absent disabled it. Default is present.
 #
 # [*host*]
-#    InfluxDB host address. Defaults to 127.0.0.1.
+#    InfluxDB host address. Default to 127.0.0.1.
 #
 # [*port*]
-#    InfluxDB HTTP port. Defaults to 8086.
+#    InfluxDB HTTP port. Default to 8086.
 #
-# [*database*] 
-#    InfluxDB database name. Defaults to icinga2.
+# [*database*]
+#    InfluxDB database name. Default to icinga2.
 #
-# [*username*] 
-#    InfluxDB user name. Defaults to none.
+# [*username*]
+#    InfluxDB user name. Default to none.
 #
-# [*password*] 
-#    InfluxDB user password. Defaults to none.
+# [*password*]
+#    InfluxDB user password. Default to none.
 #
-# [*ssl_enable*] 
-#    Whether to use a TLS stream. Defaults to false.
+# [*ssl_enable*]
+#    Whether to use a TLS stream. Default to false.
 #
-# [*ssl_ca_cert*] 
+# [*ssl_ca_cert*]
 #    CA certificate to validate the remote host.
 #
-# [*ssl_cert*] 
+# [*ssl_cert*]
 #    Host certificate to present to the remote host for mutual verification.
 #
-# [*ssl_key*] 
-#    Host key to accompany the ssl_cert
+# [*ssl_key*]
+#    Host key to accompany the ssl_cert.
 #
-# [*host_template*] 
+# [*host_template*]
 #    Host template to define the InfluxDB line protocol.
 #
-# [*service_template*] 
+# [*service_template*]
 #    Service template to define the influxDB line protocol.
 #
-# [*enable_send_thresholds*] 
+# [*enable_send_thresholds*]
 #    Whether to send warn, crit, min & max tagged data.
 #
-# [*enable_send_metadata*] 
+# [*enable_send_metadata*]
 #    Whether to send check metadata e.g. states, execution time, latency etc.
 #
-# [*flush_interval*] 
-#    How long to buffer data points before transfering to InfluxDB. Defaults to 10s.
+# [*flush_interval*]
+#    How long to buffer data points before transfering to InfluxDB. Default to 10s.
 #
-# [*flush_threshold*] 
-#    How many data points to buffer before forcing a transfer to InfluxDB. Defaults to 1024.
+# [*flush_threshold*]
+#    How many data points to buffer before forcing a transfer to InfluxDB. Default to 1024.
 #
 # === Authors
 #
@@ -67,8 +67,8 @@ class icinga2::feature::influxdb(
   $ssl_ca_cert            = undef,
   $ssl_cert               = undef,
   $ssl_key                = undef,
-  $host_template          = { host_template => { measurement => "$host.check_command$", tags => { hostname => "$host.name$" } } },
-  $service_template       = { host_template => { measurement => "$service.check_command$", tags => { hostname => "$host.name$", service => "$service.name" } } },
+  $host_template          = { measurement => '$host.check_command$', tags => { hostname => '$host.name$' } },
+  $service_template       = { measurement => '$service.check_command$', tags => { hostname => '$host.name$', service => '$service.name$' } },
   $enable_send_thresholds = false,
   $enable_send_metadata   = false,
   $flush_interval         = '10s',
@@ -87,7 +87,7 @@ class icinga2::feature::influxdb(
   validate_hash($service_template)
   validate_bool($enable_send_thresholds)
   validate_bool($enable_send_metadata)
-  validate_string($flush_interval)
+  validate_re($flush_interval, '^\d+[ms]*$')
   validate_integer($flush_threshold)
 
   icinga2::feature { 'influxdb':
