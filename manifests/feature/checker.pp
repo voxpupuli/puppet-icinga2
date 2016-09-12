31319b63d04b1530adb0431bfc74ab99126cb31d
Lennart Betz <lennart.betz@netways.de>
2016-09-12 21:19:08 +0000

    fix #12698 params inheritance in features

--- manifests/feature/checker.pp
+++ manifests/feature/checker.pp
@@ -13,7 +13,7 @@
 #
 class icinga2::feature::checker(
   $ensure = present,
-) inherits icinga2::params {
+) {
 
   validate_re($ensure, [ '^present$', '^absent$' ],
     "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
--- manifests/feature/notification.pp
+++ manifests/feature/notification.pp
@@ -13,7 +13,7 @@
 #
 class icinga2::feature::notification(
   $ensure = present,
-) inherits icinga2::params {
+) {
 
   icinga2::feature { 'notification':
     ensure => $ensure,
