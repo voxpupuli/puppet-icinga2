51a30f90be2d37f1d240593dc5883c979b52f4d7
Lennart Betz <lennart.betz@netways.de>
2016-09-13 22:53:08 +0000

    add puppet cert use for feature api

--- manifests/feature/api.pp
+++ manifests/feature/api.pp
@@ -24,6 +24,9 @@
 #
 class icinga2::feature::api(
   $ensure          = present,
+  $key             = true,
+  $cert            = true,
+  $cacert          = true,
   $accept_config   = false,
   $accept_commands = false,
 ) {
@@ -32,18 +35,56 @@ class icinga2::feature::api(
 
   validate_re($ensure, [ '^present$', '^absent$' ],
     "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
+  validate_bool($key)
+  validate_bool($cert)
+  validate_bool($cacert)
   validate_bool($accept_config)
   validate_bool($accept_commands)
 
   $conf_dir  = $::icinga2::params::conf_dir
+  $pki_dir   = $::icinga2::params::pki_dir
   $user      = $::icinga2::params::user
   $group     = $::icinga2::params::group
   $node_name = $::icinga2::_constants['NodeName']
 
-  file { "${conf_dir}/pki":
+  File {
+    owner   => $user,
+    group   => $group,
+  }
+
+  file { $pki_dir:
     ensure => directory,
-    owner  => $user,
-    group  => $group,
+  }
+
+  # host private key
+  if $key {
+    file { "${pki_dir}/${node_name}.key":
+      ensure => file,
+      mode   => $::kernel ? {
+        'windows' => undef,
+        default   => '0600',
+      },
+      source => $::settings::hostprivkey,
+      tag    => 'icinga2::config::file',
+    }
+  }
+
+  # host certificate
+  if $cert {
+    file { "${pki_dir}/${node_name}.crt":
+      ensure => file,
+      source => $::settings::hostcert,
+      tag    => 'icinga2::config::file',
+    }
+  }
+
+  # ca certificat
+  if $cacert {
+    file { "${pki_dir}/ca.crt":
+      ensure => file,
+      source => $::settings::localcacert,
+      tag    => 'icinga2::config::file',
+    }
   }
 
   icinga2::feature { 'api':
