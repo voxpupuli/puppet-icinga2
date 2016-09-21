df13d2bf2244595947534864e2d729bbdf80e1c7
Lennart Betz <lennart.betz@netways.de>
2016-09-21 17:02:26 +0000

    rewritten feature debuglog to use objects

---
+++ manifests/feature/debuglog.pp
@@ -0,0 +1,52 @@
+# == Class: icinga2::feature::debuglog
+#
+# This module configures the Icinga2 feature mainlog.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Set to present enables the feature mainlog, absent disables it. Default is present.
+#
+# [*path*]
+#   Absolute path to the log file. Default depends on platform:
+#   /var/log/icinga2/debug.log on Linux
+#   C:/ProgramData/icinga2/var/log/icinga2/debug.log on Windows
+#
+# === Authors
+#
+# Icinga Development Team <info@icinga.org>
+#
+class icinga2::feature::debuglog(
+  $ensure   = present,
+  $path     = "${::icinga2::params::log_dir}/debug.log",
+) inherits icinga2::params {
+
+  # validation
+  validate_re($ensure, [ '^present$', '^absent$' ],
+    "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
+  validate_absolute_path($path)
+
+  # compose attributes
+  $attrs = {
+    severity => 'debug',
+    path     => $path,
+  }
+
+  # create object
+  icinga2::object { "icinga2::object::FileLogger::debuglog":
+    object_name => 'debug-file',
+    object_type => 'FileLogger',
+    attrs       => $attrs,
+    target      => "${conf_dir}/features-available/debuglog.conf",
+    order       => '10',
+    notify      => $ensure ? {
+      'present' => Class['::icinga2::service'],
+      default   => undef,
+    },
+  }
+
+  # manage feature
+  icinga2::feature { 'debuglog':
+    ensure => $ensure,
+  }
+}
---
+++ spec/classes/debuglog_spec.rb
@@ -0,0 +1,140 @@
+require 'spec_helper'
+
+describe('icinga2::feature::debuglog', :type => :class) do
+  let(:pre_condition) { [
+    "class { 'icinga2': features => [], }"
+  ] }
+
+  on_supported_os.each do |os, facts|
+    let :facts do
+      facts
+    end
+
+
+    context "#{os} with ensure => present" do
+      let(:params) { {:ensure => 'present'} }
+
+      it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'present'}) }
+
+      it { is_expected.to contain_icinga2__object('icinga2::object::FileLogger::debuglog')
+        .with({ 'target' => '/etc/icinga2/features-available/debuglog.conf' })
+        .that_notifies('Class[icinga2::service]') }
+    end
+
+
+    context "#{os} with ensure => absent" do
+      let(:params) { {:ensure => 'absent'} }
+
+      it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'absent'}) }
+
+      it { is_expected.to contain_icinga2__object('icinga2::object::FileLogger::debuglog')
+        .with({ 'target' => '/etc/icinga2/features-available/debuglog.conf' }) }
+    end
+
+
+    context "#{os} with all defaults" do
+      it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'present'}) }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::FileLogger::debug-file')
+        .with({ 'target' => '/etc/icinga2/features-available/debuglog.conf' })
+        .with_content(/path = "\/var\/log\/icinga2\/debug.log"/) }
+    end
+
+
+    context "#{os} with path => /foo/bar" do
+      let(:params) { {:path => '/foo/bar'} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::FileLogger::debug-file')
+        .with({ 'target' => '/etc/icinga2/features-available/debuglog.conf' })
+        .with_content(/path = "\/foo\/bar"/) }
+    end
+
+
+    context "#{os} with path => foo/bar (not an absolute path)" do
+      let(:params) { {:path => 'foo/bar'} }
+
+      it { is_expected.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/) }
+    end
+  end
+
+
+  context 'Windows 2012 R2 with ensure => present' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:ensure => 'present'} }
+
+    it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'present'}) }
+
+    it { is_expected.to contain_icinga2__object('icinga2::object::FileLogger::debuglog')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/debuglog.conf' })
+      .that_notifies('Class[icinga2::service]') }
+  end
+
+
+  context 'Windows 2012 R2 with ensure => absent' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:ensure => 'absent'} }
+
+    it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'absent'}) }
+
+    it { is_expected.to contain_icinga2__object('icinga2::object::FileLogger::debuglog')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/debuglog.conf' }) }
+  end
+
+
+  context "Windows 2012 R2 with all defaults" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    it { is_expected.to contain_icinga2__feature('debuglog').with({'ensure' => 'present'}) }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::FileLogger::debug-file')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/debuglog.conf' })
+      .with_content(/path = "C:\/ProgramData\/icinga2\/var\/log\/icinga2\/debug.log"/) }
+  end
+
+
+  context 'Windows 2012 R2 with path => c:/foo/bar' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:path => 'c:/foo/bar'} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::FileLogger::debug-file')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/debuglog.conf' })
+      .with_content(/path = "c:\/foo\/bar"/) }
+  end
+
+
+  context 'Windows 2012 R2 with path => foo/bar (not an absolute path)' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:path => 'foo/bar'} }
+
+    it { is_expected.to raise_error(Puppet::Error, /"foo\/bar" is not an absolute path/) }
+  end
+end
