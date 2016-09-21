256b018a0dd03ce9aec134b90c394b371466642e
Lennart Betz <lennart.betz@netways.de>
2016-09-21 18:12:29 +0000

    rewritten feature gelf with objects

---
+++ manifests/feature/gelf.pp
@@ -0,0 +1,78 @@
+# == Class: icinga2::feature::gelf
+#
+# This module configures the Icinga2 feature gelf.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Set to present enables the feature gelf, absent disables it. Defaults to present.
+#
+# [*host*]
+#   GELF receiver host address. Defaults to '127.0.0.1'.
+#
+# [*port*]
+#   GELF receiver port. Defaults to 12201.
+#
+# [*source*]
+#   Source name for this instance. Defaults to icinga2.
+#
+# [*enable_send_perfdata*]
+#   Enable performance data for 'CHECK RESULT' events. Defaults to false.
+#
+# === Authors
+#
+# Icinga Development Team <info@icinga.org>
+#
+class icinga2::feature::gelf(
+  $ensure               = present,
+  $host                 = '127.0.0.1',
+  $port                 = '12201',
+  $source               = 'icinga2',
+  $enable_send_perfdata = false,
+) {
+
+  include ::icinga2::params
+
+  $conf_dir = $::icinga2::params::conf_dir
+
+  # validation
+  validate_re($ensure, [ '^present$', '^absent$' ],
+    "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
+  validate_ip_address($host)
+  validate_integer($port)
+  validate_string($source)
+  validate_bool($enable_send_perfdata)
+
+  # compose attributes
+  $attrs = {
+    host                 => $host,
+    port                 => $port,
+    source               => $source,
+    enable_send_perfdata => $enable_send_perfdata,
+  }
+
+  # create object
+  icinga2::object { "icinga2::object::GelfWriter::gelf":
+    object_name => 'gelf',
+    object_type => 'GelfWriter',
+    attrs       => $attrs,
+    target      => "${conf_dir}/features-available/gelf.conf",
+    order       => '10',
+    notify      => $ensure ? {
+      'present' => Class['::icinga2::service'],
+      default   => undef,
+    },
+  }
+
+  # import library 'notification'
+  concat::fragment { 'icinga2::feature::gelf':
+    target  => "${conf_dir}/features-available/gelf.conf",
+    content => "library \"perfdata\"\n\n",
+    order   => '05',
+  }
+
+  # manage feature
+  icinga2::feature { 'gelf':
+    ensure => $ensure,
+  }
+}
---
+++ spec/classes/gelf_spec.rb
@@ -0,0 +1,307 @@
+require 'spec_helper'
+
+describe('icinga2::feature::gelf', :type => :class) do
+  let(:pre_condition) { [
+    "class { 'icinga2': features => [], }"
+  ] }
+
+  on_supported_os.each do |os, facts|
+    let :facts do
+      facts
+    end
+
+
+    context "#{os} with ensure => present" do
+      let(:params) { {:ensure => 'present'} }
+
+      it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'present'}) }
+
+      it { is_expected.to contain_icinga2__object('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .that_notifies('Class[icinga2::service]') }
+    end
+
+
+    context "#{os} with ensure => absent" do
+      let(:params) { {:ensure => 'absent'} }
+
+      it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'absent'}) }
+
+      it { is_expected.to contain_icinga2__object('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' }) }
+    end
+
+
+    context "#{os} with all defaults" do
+      it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'present'}) }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/host = "127.0.0.1"/)
+        .with_content(/port = 12201/)
+        .with_content(/source = "icinga2"/) }
+    end
+
+
+    context "#{os} with host => 127.0.0.2" do
+      let(:params) { {:host => '127.0.0.2'} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/host = "127.0.0.2"/) }
+    end
+
+
+    context "#{os} with host => foo (not a valid IP address)" do
+      let(:params) { {:host => 'foo'} }
+
+      it { is_expected.to raise_error(Puppet::Error, /"foo" is not a valid IP address/) }
+    end
+
+
+    context "#{os} with port => 4247" do
+      let(:params) { {:port => '4247'} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/port = 4247/) }
+    end
+
+
+    context "#{os} with port => foo (not a valid integer)" do
+      let(:params) { {:port => 'foo'} }
+
+      it { is_expected.to raise_error(Puppet::Error, /first argument to be an Integer/) }
+    end
+
+
+    context "#{os} with source => foo" do
+      let(:params) { {:source => 'foo'} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/source = "foo"/) }
+    end
+
+
+    context "#{os} with source => 4247 (not a valid string)" do
+      let(:params) { {:source => 4247} }
+
+      it { is_expected.to raise_error(Puppet::Error, /4247 is not a string/) }
+    end
+
+
+    context "#{os} with enable_send_perfdata => true" do
+      let(:params) { {:enable_send_perfdata => true} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/enable_send_perfdata = true/) }
+    end
+
+
+    context "#{os} with enable_send_perfdata => false" do
+      let(:params) { {:enable_send_perfdata => false} }
+
+      it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+        .with({ 'target' => '/etc/icinga2/features-available/gelf.conf' })
+        .with_content(/enable_send_perfdata = false/) }
+    end
+
+
+    context "#{os} with enable_send_perfdata => foo (not a valid boolean)" do
+      let(:params) { {:enable_send_perfdata => 'foo'} }
+
+      it { is_expected.to raise_error(Puppet::Error, /"foo" is not a boolean/) }
+    end
+  end
+
+
+  context 'Windows 2012 R2 with ensure => present' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:ensure => 'present'} }
+
+    it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'present'}) }
+
+    it { is_expected.to contain_icinga2__object('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .that_notifies('Class[icinga2::service]') }
+  end
+
+
+  context 'Windows 2012 R2 with ensure => absent' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:ensure => 'absent'} }
+
+    it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'absent'}) }
+
+    it { is_expected.to contain_icinga2__object('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' }) }
+  end
+
+
+  context "Windows 2012 R2 with all defaults" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    it { is_expected.to contain_icinga2__feature('gelf').with({'ensure' => 'present'}) }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/host = "127.0.0.1"/)
+      .with_content(/port = 12201/)
+      .with_content(/source = "icinga2"/) }
+  end
+
+
+  context "Windows 2012 R2  with host => 127.0.0.2" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:host => '127.0.0.2'} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/host = "127.0.0.2"/) }
+  end
+
+
+  context "Windows 2012 R2 with host => foo (not a valid IP address)" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:host => 'foo'} }
+
+    it { is_expected.to raise_error(Puppet::Error, /"foo" is not a valid IP address/) }
+  end
+
+
+  context "Windows 2012 R2 with port => 4247" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:port => '4247'} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/port = 4247/) }
+  end
+
+
+  context "Windows 2012 R2 with source => 4247 (not a valid string)" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:source => 4247} }
+
+    it { is_expected.to raise_error(Puppet::Error, /4247 is not a string/) }
+  end
+
+
+  context 'Windows 2012 R2 with source => foo' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:source => 'foo'} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/source = "foo"/) }
+  end
+
+
+  context 'Windows 2012 R2 with source => 4247 (not a valid string)' do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:source => 4247} }
+
+    it { is_expected.to raise_error(Puppet::Error, /4247 is not a string/) }
+  end
+
+
+  context "Windows 2012 R2 with enable_send_perfdata => true" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:enable_send_perfdata => true} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/enable_send_perfdata = true/) }
+  end
+
+
+  context "Windows 2012 R2 with enable_send_perfdata => false" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:enable_send_perfdata => false} }
+
+    it { is_expected.to contain_concat__fragment('icinga2::object::GelfWriter::gelf')
+      .with({ 'target' => 'C:/ProgramData/icinga2/etc/icinga2/features-available/gelf.conf' })
+      .with_content(/enable_send_perfdata = false/) }
+  end
+
+
+  context "Windows 2012 R2 with enable_send_perfdata => foo (not a valid boolean)" do
+    let(:facts) { {
+      :kernel => 'Windows',
+      :architecture => 'x86_64',
+      :osfamily => 'Windows',
+      :operatingsystem => 'Windows',
+      :operatingsystemmajrelease => '2012 R2'
+    } }
+    let(:params) { {:enable_send_perfdata => 'foo'} }
+
+    it { is_expected.to raise_error(Puppet::Error, /"foo" is not a boolean/) }
+  end
+end
