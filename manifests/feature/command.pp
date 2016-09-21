ecd9adb97c44e4ac5ab596868c0ca34eecdbaeef
Lennart Betz <lennart.betz@netways.de>
2016-09-21 15:05:50 +0000

    rewritten feature command to use objects

---
+++ manifests/feature/command.pp
@@ -0,0 +1,56 @@
+# == Class: icinga2::feature::command
+#
+# This module configures the Icinga2 feature command.
+#
+# === Parameters
+#
+# [*ensure*]
+#   Set to present to enable the feature command, absent to disabled it. Default is present.
+#
+# [*command_path*]
+#   Absolute path to the command pipe. Default depends on platform, /var/run/icinga2/cmd/icinga2.cmd on Linux
+#   and C:/ProgramData/icinga2/var/run/icinga2/cmd/icinga2.cmd on Windows.
+#
+# === Authors
+#
+# Icinga Development Team <info@icinga.org>
+#
+class icinga2::feature::command(
+  $ensure       = present,
+  $command_path = "${::icinga2::params::run_dir}/cmd/icinga2.cmd",
+) inherits icinga2::params {
+
+  validate_re($ensure, [ '^present$', '^absent$' ],
+    "${ensure} isn't supported. Valid values are 'present' and 'absent'.")
+  validate_absolute_path($command_path)
+
+  # compose attributes
+  $attrs = {
+    command_path => $command_path,
+  }
+
+  # create object
+  icinga2::object { "icinga2::object::ExternalCommandListener::command":
+    object_name => 'command',
+    object_type => 'ExternalCommandListener',
+    attrs       => $attrs,
+    target      => "${conf_dir}/features-available/command.conf",
+    order       => '10',
+    notify      => $ensure ? {
+      'present' => Class['::icinga2::service'],
+      default   => undef,
+    },
+  }
+
+  # import library 'compat'
+  concat::fragment { 'icinga2::feature::command':
+    target  => "${conf_dir}/features-available/command.conf",
+    content => "library \"compat\"\n\n",
+    order   => '05',
+  }
+
+  # manage feature
+  icinga2::feature { 'command':
+    ensure => $ensure,
+  }
+}
