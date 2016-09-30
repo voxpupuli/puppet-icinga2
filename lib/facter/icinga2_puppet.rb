52276a8c38c6dc66b51c6ffe6c9220ad402f272a
Lennart Betz <lennart.betz@netways.de>
2016-09-30 09:26:07 +0200

    fix facts with shorter names

--- lib/facter/icinga2_puppet.rb
+++ lib/facter/icinga2_puppet.rb
@@ -2,19 +2,19 @@ require 'facter'
 
 Puppet.settings.preferred_run_mode= :agent
 
-Facter.add(:icinga2_puppet_setting_hostcert) do
+Facter.add(:icinga2_puppet_hostcert) do
   setcode do
     Puppet.settings[:hostcert]
   end
 end
 
-Facter.add(:icinga2_puppet_setting_hostprivkey) do
+Facter.add(:icinga2_puppet_hostprivkey) do
   setcode do
     Puppet.settings[:hostprivkey]
   end
 end
 
-Facter.add(:icinga2_puppet_setting_localcacert) do
+Facter.add(:icinga2_puppet_localcacert) do
   setcode do
     Puppet.settings[:localcacert]
   end
