165a2f37b046adafa949440af5e53efac7421c78
Blerim Sheqa <blerim.sheqa@netways.de>
2016-11-10 13:31:42 +0100

    Require certain parameters for object notificationcommand

--- manifests/object/notificationcommand.pp
+++ manifests/object/notificationcommand.pp
@@ -49,7 +49,6 @@
 #
 define icinga2::object::notificationcommand (
   $ensure     = present,
-  $execute    = undef,
   $command    = undef,
   $env        = undef,
   $vars       = undef,
@@ -72,8 +71,7 @@ define icinga2::object::notificationcommand (
   validate_absolute_path($target)
   validate_string($order)
 
-  if $execute { validate_string ($execute) }
-  if $command { validate_string ($command) }
+  validate_array($command)
   if $env { validate_hash ($env) }
   if $vars { validate_hash ($vars) }
   if $timeout { validate_integer ($timeout) }
@@ -81,7 +79,6 @@ define icinga2::object::notificationcommand (
 
   # compose attributes
   $attrs = {
-    'execute' => $execute,
     'command' => $command,
     'env' => $env,
     'vars' => $vars,
