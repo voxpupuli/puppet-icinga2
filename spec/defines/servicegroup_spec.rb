dd976716f8a3b373e7d4e4f95005a64bcb313039
Lennart Betz <lennart.betz@netways.de>
2017-01-17 21:06:05 +0100

    fix object servicegroup

--- spec/defines/servicegroup_spec.rb
+++ spec/defines/servicegroup_spec.rb
@@ -19,6 +19,7 @@ describe('icinga2::object::servicegroup', :type => :define) do
       it { is_expected.to contain_concat__fragment('icinga2::object::ServiceGroup::bar')
                               .with({'target' => '/bar/baz'})
                               .with_content(/object ServiceGroup "bar"/)
+                              .without_content(/groups =/)
                               .without_content(/assign where/)
                               .without_content(/ignore where/) }
 
@@ -54,10 +55,12 @@ describe('icinga2::object::servicegroup', :type => :define) do
     end
 
 
-    context "#{os} with groups => foo (not a valid array)" do
+    context "#{os} with groups => foo" do
       let(:params) { {:groups => 'foo', :target => '/bar/baz'} }
 
-      it { is_expected.to raise_error(Puppet::Error, /"foo" is not an Array/) }
+      it { is_expected.to contain_concat__fragment('icinga2::object::ServiceGroup::bar')
+                              .with({'target' => '/bar/baz'})
+                              .with_content(/groups = \[ "foo", \]/) }
     end
   end
 end
@@ -94,6 +97,7 @@ describe('icinga2::object::servicegroup', :type => :define) do
     it { is_expected.to contain_concat__fragment('icinga2::object::ServiceGroup::bar')
                             .with({'target' => 'C:/bar/baz'})
                             .with_content(/object ServiceGroup "bar"/)
+                            .without_content(/groups =/)
                             .without_content(/assign where/)
                             .without_content(/ignore where/) }
 
@@ -129,9 +133,11 @@ describe('icinga2::object::servicegroup', :type => :define) do
   end
 
 
-  context "Windows 2012 R2 with groups => foo (not a valid array)" do
+  context "Windows 2012 R2 with groups => foo" do
     let(:params) { {:groups => 'foo', :target => 'C:/bar/baz'} }
 
-    it { is_expected.to raise_error(Puppet::Error, /"foo" is not an Array/) }
+    it { is_expected.to contain_concat__fragment('icinga2::object::ServiceGroup::bar')
+                            .with({'target' => 'C:/bar/baz'})
+                            .with_content(/groups = \[ "foo", \]/) }
   end
 end
