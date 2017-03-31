31d076d8fd92eb29d2733f7ba0b5e7f2d950aec7
Tomas Barton <barton.tomas@gmail.com>
2017-03-31 18:09:16 +0200

    fix time periods example

--- examples/example_config.pp
+++ examples/example_config.pp
@@ -315,13 +315,13 @@ file { '/etc/icinga2/example.d':
   import       => [ 'legacy-timeperiod' ],
   display_name => 'Icinga 2 9to5 TimePeriod',
   ranges       => {
-    monday    => '00:09-17:00',
-    tuesday   => '00:09-17:00',
-    wednesday => '00:09-17:00',
-    thursday  => '00:09-17:00',
-    friday    => '00:09-17:00',
-    saturday  => '00:09-17:00',
-    sunday    => '00:09-17:00',
+    monday    => '09:00-17:00',
+    tuesday   => '09:00-17:00',
+    wednesday => '09:00-17:00',
+    thursday  => '09:00-17:00',
+    friday    => '09:00-17:00',
+    saturday  => '09:00-17:00',
+    sunday    => '09:00-17:00',
   },
 }
 
