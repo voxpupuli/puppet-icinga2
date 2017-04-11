bd763e04bdaa80f1777e6b821d5d597811e8799f
Lennart Betz <lennart.betz@netways.de>
2017-04-11 07:13:17 +0200

    expand example for feature idomysql

--- examples/init_idomysql.pp
+++ examples/init_idomysql.pp
@@ -1,5 +1,21 @@
-class { 'icinga2':
-  manage_repo => true
+include ::mysql::server
+
+mysql::db { 'icinga2':
+  user     => 'icinga2',
+  password => 'supersecret',
+  host     => 'localhost',
+  grant    => ['SELECT', 'INSERT', 'UPDATE', 'DELETE', 'DROP', 'CREATE VIEW', 'CREATE', 'INDEX', 'EXECUTE', 'ALTER'],
 }
 
-include icinga2::feature::idomysql
+class { '::icinga2':
+  manage_repo => true,
+}
+
+class{ '::icinga2::feature::idomysql':
+  user          => 'icinga2',
+  password      => 'supersecret',
+  database      => 'icinga2',
+  import_schema => true,
+  require       => Mysql::Db['icinga2'],
+}
+
