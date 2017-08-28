8320f30aee9085b9ebfd5212674ba42fce94a7c3
Markus Frosch <markus.frosch@icinga.com>
2017-08-28 16:10:26 +0200

    Stub assert_private function for spec testing

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,4 +10,9 @@ RSpec.configure do |c|
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_facts.yml', __FILE__))
   default_facts.merge!(YAML.load(File.read(File.expand_path('../default_module_facts.yml', __FILE__)))) if File.exist?(File.expand_path('../default_module_facts.yml', __FILE__))
   c.default_facts = default_facts
+
+  c.before(:each) do
+    # Stub assert_private function from stdlib to not fail within this test
+    Puppet::Parser::Functions.newfunction(:assert_private) { |_| }
+  end
 end
