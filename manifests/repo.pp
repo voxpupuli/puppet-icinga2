4eb296084d0591c2d310bfd53b64e8f08636a5ef
Blerim Sheqa <blerim.sheqa@icinga.com>
2017-10-06 09:02:15 +0200

    User service pack repository for SLES
    closes #386

--- manifests/repo.pp
+++ manifests/repo.pp
@@ -91,7 +91,7 @@ class icinga2::repo {
         case $::operatingsystem {
           'SLES': {
             zypprepo { 'icinga-stable-release':
-              baseurl  => "http://packages.icinga.com/SUSE/${::operatingsystemmajrelease}/release/",
+              baseurl  => "http://packages.icinga.com/SUSE/${::operatingsystemrelease}/release/",
               enabled  => 1,
               gpgcheck => 1,
               require  => Exec['import icinga gpg key']
