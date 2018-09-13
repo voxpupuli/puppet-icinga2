cdfee54b8e8bef5bcb509ae24e5847a0e3e04647
Lennart Betz <lennart.betz@netways.de>
2018-09-13 19:28:34 +0200

    rework api example, add ca

--- examples/init_api.pp
+++ examples/init_api.pp
@@ -2,8 +2,22 @@ class { 'icinga2':
   manage_repo => true,
 }
 
-include icinga2::feature::api
+class { '::icinga2::feature::api':
+  pki => none,
+}
 
-#icinga2::object::zone { 'global-templates':
-#  global => true,
-#}
+include ::icinga2::pki::ca
+
+::icinga2::object::apiuser { 'director':
+  ensure      => present,
+  password    => 'Eih5Weefoo2oa8sh', 
+  permissions => [ "*" ],
+  target      => '/etc/icinga2/conf.d/api-users.conf',
+}
+
+::icinga2::object::apiuser { 'icingaweb2':
+  ensure      => present,
+  password    => '12e2ef553068b519',
+  permissions => [ 'status/query', 'actions/*', 'objects/modify/*', 'objects/query/*' ],
+  target      => '/etc/icinga2/conf.d/api-users.conf',
+}
