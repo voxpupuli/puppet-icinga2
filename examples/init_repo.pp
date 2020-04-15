8eed0c47c238a2f84e54f4af37983ee1056969e8
Lennart Betz <lennart.betz@netways.de>
2020-04-15 08:41:41 +0200

    replace own class repo with icinga::repos

--- examples/init_repo.pp
+++ examples/init_repo.pp
@@ -1,17 +1,21 @@
 case $::facts['os']['name'] {
   'redhat', 'centos': {
     if Integer($::facts['os']['release']['major']) < 8 {
-      package { 'epel-release':
-        ensure => installed,
-        before => Class['icinga2'],
-      }
+      $epel      = true
+      $backports = false
     }
   } # RedHat
   'debian', 'ubuntu': {
     if $::facts['os']['distro']['codename'] in [ 'stretch', 'trusty' ] {
-      include ::apt, ::apt::backports
+      $epel      = false
+      $backports = true
     }
   } # Debian
+}
+
+class { '::icinga::repos':
+  manage_epel         => $epel,
+  configure_backports => $backports,
 }
 
 class { 'icinga2':
