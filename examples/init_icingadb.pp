543b20ebad013e78719ff6585004e7bc886504e3
Lennart Betz <lennart.betz@netways.de>
2020-04-18 13:25:01 +0200

    add example for icingadb feature

--- examples/init_icingadb.pp
+++ examples/init_icingadb.pp
@@ -1,3 +1,25 @@
+case $::facts['os']['name'] {
+  'redhat', 'centos': {
+    if Integer($::facts['os']['release']['major']) < 8 {
+      $epel      = true
+      $backports = false
+    }
+  } # RedHat
+  'debian', 'ubuntu': {
+    if $::facts['os']['distro']['codename'] in [ 'stretch', 'trusty' ] {
+      $epel      = false
+      $backports = true
+    }
+  } # Debian
+}
+
+class { '::icinga::repos':
+  manage_release      => false,
+  manage_testing      => true,
+  manage_epel         => $epel,
+  configure_backports => $backports,
+}
+
 include ::icinga2
 include ::icinga2::feature::icingadb
 
