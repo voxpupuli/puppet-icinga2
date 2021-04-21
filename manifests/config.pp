6d435f7c203386e116fbeb7b5f822b0ddbe30b0e
Lennart Betz <lennart.betz@netways.de>
2021-04-21 10:54:08 +0200

    fix #648 Manage owner, group and permission of config files

--- manifests/config.pp
+++ manifests/config.pp
@@ -9,6 +9,8 @@ class icinga2::config {
 
   $constants      = prefix($::icinga2::_constants, 'const ')
   $conf_dir       = $::icinga2::globals::conf_dir
+  $user           = $::icinga2::globals::user
+  $group          = $::icinga2::globals::group
   $plugins        = $::icinga2::plugins
   $confd          = $::icinga2::_confd
   $purge_features = $::icinga2::purge_features
@@ -19,6 +21,12 @@ class icinga2::config {
   } else {
     $template_constants  = regsubst(icinga2_attributes($constants), '\n', "\r\n", 'EMG')
     $template_mainconfig = regsubst(template('icinga2/icinga2.conf.erb'), '\n', "\r\n", 'EMG')
+  }
+
+  File {
+    owner => $user,
+    group => $group,
+    mode  => '0640',
   }
 
   file { "${conf_dir}/constants.conf":
