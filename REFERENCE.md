ef9d5f3200452140cc077e7fd99241435a3eed60
Lennart Betz <lennart.betz@netways.de>
2023-09-08 13:28:11 +0200

    fix #736 drop concurrent_checks parameter from feature checker

--- REFERENCE.md
+++ REFERENCE.md
@@ -659,15 +659,11 @@ Default value: `undef`
 
 Configures the Icinga 2 feature checker.
 
-* **Note** Deprecated in Icinga 2.11, replaced by global constant
-MaxConcurrentChecks which will be set if you still use concurrent_checks.
-
 #### Parameters
 
 The following parameters are available in the `icinga2::feature::checker` class:
 
 * [`ensure`](#-icinga2--feature--checker--ensure)
-* [`concurrent_checks`](#-icinga2--feature--checker--concurrent_checks)
 
 ##### <a name="-icinga2--feature--checker--ensure"></a>`ensure`
 
@@ -676,14 +672,6 @@ Data type: `Enum['absent', 'present']`
 Set to present enables the feature checker, absent disabled it.
 
 Default value: `present`
-
-##### <a name="-icinga2--feature--checker--concurrent_checks"></a>`concurrent_checks`
-
-Data type: `Optional[Integer[1]]`
-
-The maximum number of concurrent checks.
-
-Default value: `undef`
 
 ### <a name="icinga2--feature--command"></a>`icinga2::feature::command`
 
--- manifests/feature/checker.pp
+++ manifests/feature/checker.pp
@@ -4,15 +4,8 @@
 # @param ensure
 #   Set to present enables the feature checker, absent disabled it.
 #
-# @param concurrent_checks
-#   The maximum number of concurrent checks.
-#
-# @note Deprecated in Icinga 2.11, replaced by global constant
-#   MaxConcurrentChecks which will be set if you still use concurrent_checks.
-#
 class icinga2::feature::checker (
   Enum['absent', 'present'] $ensure            = present,
-  Optional[Integer[1]]      $concurrent_checks = undef,
 ) {
   if ! defined(Class['icinga2']) {
     fail('You must include the icinga2 base class before using any icinga2 feature class!')
@@ -24,17 +17,10 @@ class icinga2::feature::checker (
     default   => undef,
   }
 
-  # compose attributes
-  $attrs = {
-    concurrent_checks => $concurrent_checks,
-  }
-
   # create object
   icinga2::object { 'icinga2::object::CheckerComponent::checker':
     object_name => 'checker',
     object_type => 'CheckerComponent',
-    attrs       => delete_undef_values($attrs),
-    attrs_list  => keys($attrs),
     target      => "${conf_dir}/features-available/checker.conf",
     order       => 10,
     notify      => $_notify,
