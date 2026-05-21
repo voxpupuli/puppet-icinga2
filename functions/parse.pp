ea4516073c8d54f833cf27890cac34802d0d7d20
Louis Charreau <louis.charreau@vadesecure.com>
2026-05-21 15:22:10 +0200

    Handle icinga2 constants when class is not declared

--- functions/parse.pp
+++ functions/parse.pp
@@ -28,7 +28,7 @@ function icinga2::parse(
   icinga2::icinga2_attributes(
     $attrs,
     concat($icinga2::globals::reserved, $reserved),
-    pick($icinga2::_constants, $icinga2::globals::constants) + $constants,
+    pick(getvar('icinga2::_constants'), $icinga2::globals::constants) + $constants,
     $indent
   )
 }
